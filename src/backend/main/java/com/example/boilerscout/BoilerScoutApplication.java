a21e441463f4cb2f677e4446ad04a90daf0cd258
Terry Lam <tlam2297@gmail.com>
2018-01-29 14:50:48 -0500

    CHORE: Create initial project space

---
+++ src/backend/main/java/com/example/boilerscout/BoilerScoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.boilerscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerScoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerScoutApplication.class, args);
+	}
+
+}
