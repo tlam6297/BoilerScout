72c06fc488afb04df57496fb0f44ba9ce6261950
Selin Ovali <sovali@purdue.edu>
2018-02-08 19:52:29 -0500

    Extend App.js, handle authorization and unauthorization

--- src/frontend/boilerscout/src/App.js
+++ src/frontend/boilerscout/src/App.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      userid: null,
+    }
+  }
+  // Return user id if signed in, null if not
+  signedIn = () => {
+    return this.state.uid
+  }
+  // handle auth, set userid
+  handleAuth = (user) => {
+    localStorage.setItem('userid', user.uid)
+    this.setState( 
+      {uid: user.uid}
+    )
+  }
+  // handle unauth, remove userid
+  handleUnauth = () => {
+    localStorage.removeItem('uid')
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">BoilerScout</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to BoilerScout y'all
         </p>
       </div>
     );
