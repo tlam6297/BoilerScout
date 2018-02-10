f4b8f9d57f2ed7c69ac390b607560598b34eaeac
Selin Ovali <sovali@purdue.edu>
2018-02-09 19:24:57 -0500

    Add Main component that works

---
+++ src/frontend/boilerscout/src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="Home">
+            <header className="Header">
+             <span className="title"> This is Main</span>
+             
+            </header>
+        </div>
+    )
+
+}
+
+export default Main;
