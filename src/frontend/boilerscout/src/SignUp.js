5a27df28e2b2dd4659694924f5991bf3b3c282fa
Selin Ovali <sovali@purdue.edu>
2018-02-13 17:31:12 -0500

    Delete ForgotPassword files, construct SignUp.js

---
+++ src/frontend/boilerscout/src/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SignUp = () => {
+
+    return (
+        <div className="SignUp">
+             <div className="SignUpButton">
+             <button href="#" > 
+              SIGN UP
+             </button>
+             </div>
+        </div>
+    )
+
+}
+
+export default SignUp;
