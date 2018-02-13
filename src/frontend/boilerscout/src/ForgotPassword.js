64ea18a0a47a2d0ae3a22e119b67f5b40165068b
Jacob Miecznikowski <jmieczni@purdue.edu>
2018-02-13 18:43:12 -0500

    FEAT: Add form to ForgotPassword and Check for valid email

--- src/frontend/boilerscout/src/ForgotPassword.js
+++ src/frontend/boilerscout/src/ForgotPassword.js
@@ -1,15 +1,63 @@
-import React, { Component } from 'react';
-import './ForgotPassword.css';
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { BrowserRouter as Router} from 'react-router-dom'
+import "./ForgotPassword.css"
 
 class ForgotPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ""
+    };
+  }
+
+  validateForm = () => {
+    const email = this.state.email.toLowerCase();
+    const regex = /^\S+@purdue.edu$/;
+    const validEmail = regex.test(email);
+
+    console.log("Good email?: " + validEmail);
+    return (validEmail);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
+
   render() {
     return (
-      <div className="ForgotPassword">
-      { /* Filler link for now */ } 
-        <a href="https://www.google">Forgot Password?</a> 
-      </div>
+      <Router>
+        <div className="ForgotPassword">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" bsSize="large">
+              <ControlLabel>Email:</ControlLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <Button
+              block
+              bsSize="small"
+              disabled={!this.validateForm()}
+              type="submit">
+              Submit
+            </Button>
+          </form>
+        </div>
+      </Router>
     );
   }
 }
 
-export default ForgotPassword;
+export default ForgotPassword
