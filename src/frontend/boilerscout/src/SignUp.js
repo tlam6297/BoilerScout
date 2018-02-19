a1a1bd4b4f546660508735bd4a7ab1992cef6122
Jacob Miecznikowski <jmieczni@purdue.edu>
2018-02-18 21:24:17 -0500

    Create SignUp.js

---
+++ src/frontend/boilerscout/src/SignUp.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react'
+import { BrowserRouter as Router, Link, Redirect} from 'react-router-dom'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import './SignUp.css'
+
+class SignUp extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            redirect: false,
+        };
+    }
+
+    validateForm = () => {
+        const email = this.state.email.toLowerCase();
+        const password = this.state.password;
+        const regex = /^\S+@purdue.edu$/;
+        // Add password test
+        const validEmail = regex.test(email);
+        console.log("Good email?: " + validEmail);
+        return (validEmail);
+    }
+
+    handleChange = (event) => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({ redirect: true })
+      }
+
+    render () {
+    return (
+        <div className="SignUp">
+            <form onSubmit={this.handleSubmit}>
+            <div className="Form">
+            <FormGroup controlId="signup" bsSize="large">
+              <ControlLabel>Email:</ControlLabel>
+              <FormControl
+                className="FormInput"
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password:</ControlLabel>
+             <FormControl
+                className="FormInput"
+                autoFocus
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              </FormGroup>
+              </div>
+            <Button
+              block
+              bsSize="small"
+              disabled={!this.validateForm()}
+              type="submit">
+              SUBMIT        
+            </Button>
+            </form>
+        </div>
+    )
+    }
+
+}
+
+export default SignUp;
