576301a180fa6b3fd7d02030dc6fca75ca916b0a
Jacob Miecznikowski <jmieczni@purdue.edu>
2018-02-20 22:29:23 -0500

    Create UpdatePassword

---
+++ src/frontend/boilerscout/src/UpdatePassword.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import { BrowserRouter as Route, Router, Link, Redirect} from 'react-router-dom'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import './AdvancedFilters.css';
+
+class UpdatePassword extends Component {
+
+  render() {
+    return(
+      <div className="UpdatePassword">
+          
+      </div>
+    )
+  }
+}
