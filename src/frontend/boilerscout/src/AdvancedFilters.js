d0962697e7d0790eb7e72ffbf5b8ee892474d8f5
Jacob Miecznikowski <jmieczni@purdue.edu>
2018-02-20 20:28:11 -0500

    Create AdvanedFilters.js

---
+++ src/frontend/boilerscout/src/AdvancedFilters.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import { BrowserRouter as Route, Router, Link, Redirect} from 'react-router-dom'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
+class AdvancedFilters extends Component {
+  constructor (props) {
+      super(props);
+
+      this.state = {
+      };
+  }
+
+  render() {
+      return (
+          <div className="Advanced Filters">
+            <p>Hello world</p>
+          </div>
+      )
+  }
+
+}
+
+export default Login;
