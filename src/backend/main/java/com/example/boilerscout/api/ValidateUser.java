455b064a4733977d4af9837893a8f39d25eaabdb
Terry Lam <tlam2297@gmail.com>
2018-02-24 17:13:07 -0500

    FEAT: Write new utility function to check for expired JWTs

--- src/backend/main/java/com/example/boilerscout/api/ValidateUser.java
+++ src/backend/main/java/com/example/boilerscout/api/ValidateUser.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.DatatypeConverter;
+import java.util.Date;
 
 /**
  * Created by terrylam on 2/19/18.
@@ -30,7 +31,7 @@ public class ValidateUser {
      * @return
      */
 
-    public static boolean validateToken(String jwt, String userId) {
+    public static boolean isValidToken(String jwt, String userId) {
         try {
             Claims claims = Jwts.parser()
                     .setSigningKey(DatatypeConverter.parseBase64Binary("TerryLam"))
@@ -45,5 +46,23 @@ public class ValidateUser {
             return false;
         }
         return true;
+    }
+
+    public static boolean isExpiredToken(String jwt) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(DatatypeConverter.parseBase64Binary("TerryLam"))
+                    .parseClaimsJws(jwt).getBody();
+            Date expiration = claims.getExpiration();
+            //If the expiration Date is a time BEFORE the current time, it is expired
+            if (expiration.before(new Date())) {
+                return true;
+            }
+        } catch (JwtException ex) {
+            log.info("Error validating token. Exception Message " + ex.getMessage());
+            return true;
+        } 
+        return false;
+        }
     }
 }
