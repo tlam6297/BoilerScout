484681df2dfcfabd3482aaf3eadf1d0f9d757fef
Terry Lam <tlam2297@gmail.com>
2018-03-01 16:13:22 -0500

    FEAT: User validation

--- src/backend/main/java/com/example/boilerscout/api/ProfileController.java
+++ src/backend/main/java/com/example/boilerscout/api/ProfileController.java
@@ -1,9 +1,11 @@
 package com.example.boilerscout.api;
 
+import io.jsonwebtoken.JwtException;
 import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,11 +34,22 @@ public class ProfileController extends ValidateUser {
         String userId = body.get("userId").toString();
         String token = body.get("token").toString();
 
+        try {
+            isValidToken(token, userId);
+            isExpiredToken(token);
+        } catch (JwtException ex) {
+            log.info("Exception Message" + ex.getMessage());
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new RuntimeException("[InternalServerError] - Error validating JWT.");
+        }
 
 
 
 
 
+
+
+        response.put("status", "This is a valid call!!");
         return response;
     }
 
