8ba363a287f5a7b254a9a5f762fcb6f6d3b23bc1
Terry Lam <tlam2297@gmail.com>
2018-03-06 14:57:29 -0500

    FIX: Update and fix unit tests

--- src/test/java/com/example/boilerscout/api/ValidationUtilitySpec.java
+++ src/test/java/com/example/boilerscout/api/ValidationUtilitySpec.java
@@ -23,31 +23,34 @@ public class ValidationUtilitySpec extends ValidationUtility {
 
     @Test
     public void testTokenValidation() {
-        String jwt = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsYW00NUBwdXJkdWUuZWR1IiwidXNlcklkIjoiZDYyNDc3OTMtYTgzMi00NjhmLWJ" +
-                "jZDMtYTcyM2RiOTE2YThkIiwiZXhwIjoxNTE5OTM5NDU2fQ.3E6v85WD8YbZdtHUmHWsGRbvVBXIviegOVOKa_kNkik3XTlxsrqn5GG" +
-                "Em41NtNr89i1FwMS9Y987qgp-KQ4oBQ";
-        String userId = "d6247793-a832-468f-bcd3-a723db916a8d";
-        Assert.assertTrue("This token is valid!", isValidToken(jwt, userId));
+        String JWT = Jwts.builder()
+                .setSubject("someEmail@purdue.edu")
+                .claim("userId", "someUserId")
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME)) //This is about 6 years
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+        String userId = "someUserId";
+        Assert.assertTrue("This token is valid!", isValidToken(JWT, userId));
     }
 
-//    @Test
-//    public void testTokenExpiration() {
-//        //Doesn't expire until 2/24/2050
-//        String validJWT = Jwts.builder()
-//                .setSubject("someEmail@purdue.edu")
-//                .claim("userId", "someUserId")
-//                .setExpiration(new Date(System.currentTimeMillis() + (EXPIRATIONTIME * 1000000))) //This is about 6 years
-//                .signWith(SignatureAlgorithm.HS512, SECRET)
-//                .compact();
-//        String expiredJWT = Jwts.builder()
-//                .setSubject("someEmail@purdue.edu")
-//                .claim("userId", "someUserId")
-//                .setExpiration(new Date(System.currentTimeMillis() - 86400000)) //This is yesterday
-//                .signWith(SignatureAlgorithm.HS512, SECRET)
-//                .compact();
-//        Assert.assertTrue("This token is expired!", isExpiredToken(expiredJWT));
-//        Assert.assertFalse("This token is not expired!", isExpiredToken(validJWT));
-//    }
+    @Test
+    public void testTokenExpiration() {
+        //Doesn't expire until 2/24/2050
+        String validJWT = Jwts.builder()
+                .setSubject("someEmail@purdue.edu")
+                .claim("userId", "someUserId")
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME)) //This is about 6 years
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+        String expiredJWT = Jwts.builder()
+                .setSubject("someEmail@purdue.edu")
+                .claim("userId", "someUserId")
+                .setExpiration(new Date(System.currentTimeMillis() - 86400000)) //This is yesterday
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+        Assert.assertTrue("This token is expired!", isExpiredToken(expiredJWT));
+        Assert.assertFalse("This token is not expired!", isExpiredToken(validJWT));
+    }
 
     @Test
     public void testExistingSkill() {
