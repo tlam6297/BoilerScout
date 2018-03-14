3d45417f19f5de1acb84035193eb7f3ccc60efcf
Jacob Miecznikowski <jmieczni@purdue.edu>
2018-03-13 22:40:20 -0400

    Add return home link to password sent

--- src/frontend/boilerscout/src/PasswordSent.js
+++ src/frontend/boilerscout/src/PasswordSent.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
 
 class PasswordSent extends Component {
   render() {
     return (
       <div className="PasswordSent" class="Form">                
         <h1>Password Sent!</h1>
+        <Link to="/">Return Home</Link>
       </div>
     );
   }
