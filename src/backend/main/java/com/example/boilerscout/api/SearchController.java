db0c4cbd3c9b2ac379fbd31e54fe6099fb8005a8
Terry Lam <tlam2297@gmail.com>
2018-03-18 18:28:02 -0400

    FEAT: Add searching for skills

--- src/backend/main/java/com/example/boilerscout/api/SearchController.java
+++ src/backend/main/java/com/example/boilerscout/api/SearchController.java
@@ -10,8 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by terrylam on 3/15/18.
@@ -32,7 +31,6 @@ public class SearchController extends ValidationUtility {
 
         Map<String, Object> response = new HashMap<String, Object>();
 
-
         if (!isValidToken(token, userId) || isExpiredToken(token)) {
             response.put("status", HttpStatus.INTERNAL_SERVER_ERROR + " - This token is not valid!");
             return response;
@@ -40,12 +38,30 @@ public class SearchController extends ValidationUtility {
             //search
             try {
                 if(type.equals("name")) {
-
+                    List<Map<String, Object>> queryOfUsers = jdbcTemplate.queryForList("SELECT * FROM profiles WHERE full_name LIKE '%" + query + "%'");
+                    response.put("query", queryOfUsers);
                 } else if(type.equals("skill")) {
+                    List<String> skillIds = jdbcTemplate.queryForList("SELECT skill_id FROM skills WHERE skill_name LIKE '%" + query + "%'", String.class);
+                    HashSet<String> userIdsWithSkill = new HashSet<String>();
+                    //get all the userIds associated with the skills
+                    for(int i = 0; i < skillIds.size(); i++) {
+                        HashSet<String> hs = new HashSet<String>(jdbcTemplate.queryForList("SELECT user_id FROM user_skills WHERE skill_id='" + skillIds.get(i) + "'", String.class));
+                        userIdsWithSkill.addAll(hs);
+                    }
 
-                } else { //type is a course
-
+                    //now need to return fullname with the userId (maybe the profiles?)
+                    List<Map<String, Object>> result = new ArrayList<Map<String,Object>>();
+                    for(Iterator<String> it = userIdsWithSkill.iterator(); it.hasNext();) {
+                        String uid = it.next();
+                        result.addAll(jdbcTemplate.queryForList("SELECT * FROM profiles WHERE user_id='" + uid + "'"));
+                    }
+                    response.put("query", result);
                 }
+                else { //type is a course
+                    List<String> courseIds = jdbcTemplate.queryForList("SELECT course_id FROM courses WHERE course_name LIKE '%" + query + "%'", String.class);
+                    response.put("query", courseIds);
+
+             }
 
             } catch (DataAccessException ex) {
                 log.info("Exception Message" + ex.getMessage());
