4a686affabf41d5db6c5ff558031603e378d7696
Selin Ovali <selinovali@pal-nat186-119-122.itap.purdue.edu>
2018-04-01 15:42:41 -0400

    Community render links to each forum

--- src/frontend/boilerscout/src/Community.js
+++ src/frontend/boilerscout/src/Community.js
@@ -5,54 +5,78 @@ import NavBar from './TopNavBar.js'
 import './Community.css'
 
 class Community extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            token: "",
+            userId: "",
+            forumId: "",
+            forums: [],
+        }
+    }
+
+    componentDidMount= () => {
+        const all_data = this.props.location.search;
+        const split = all_data.split("&");
+
+        const real_id_with_equals = split[0].substring(1);
+        const rea = real_id_with_equals.split("=");
+
+        const real_id = rea[1];
+
+        const user_id = localStorage.getItem("id");
+        const token = localStorage.getItem("token");
+
+        const url = "http://localhost:8080/community?userId=" + user_id + "&token=" + token;
+
+        axios.get(url)
+        .then(res => {
+            //console.log(res.data);
+            if (res.status == 200) {
+                this.setState({
+                    forums: res.data.forums,
+                    userId: res.data.userId,
+                    token: res.data.token,
+                });        
+            } else {
+                alert("Invalid Token- Please login again");
+                this.setState({
+                    redirect: true,
+                })
+            }      
+        });
+
+
+    }
   render() {
     return (
       <div className="Community">
         <div className="NavBar">
           <NavBar/>
         </div>
-        <div class="grid-container-comm">
-            <h2> Community </h2>
-            <div class="grid-item">
-                <Route render={({ history }) => (
-                    <button
-                    type='button'
-                    onClick={() => { history.push('/class-help') }}
-                    >
-                        Class Help
-                    </button>
-                )} />
-            </div>
-            <div class="grid-item">
-                <Route render={({ history }) => (
-                    <button
-                    type='button'
-                    onClick={() => { history.push('/mentor-search') }}
-                    >
-                       Mentor Search
-                    </button>
-                )} />
-            </div>
-            <div class="grid-item">
-                <Route render={({ history }) => (
-                    <button
-                    type='button'
-                    onClick={() => { history.push('/project-ideas') }}
-                    >
-                       Project Ideas
-                    </button>
-                )} />
-            </div>
-            <div class="grid-item">
-                <Route render={({ history }) => (
-                    <button
-                    type='button'
-                    onClick={() => { history.push('/tutor-search') }}
-                    >
-                       Tutor Search
-                    </button>
-                )} />
-            </div>
+        <h2> Community </h2>
+        <div class="grid-container-comm forums">
+        <ul>
+          <div className='li'>
+            {this.state.forums.map((result, index) =>
+              <Link to={
+                  `/community/get-threads?userId=` + result.user_id + `&token=` + result.token + `&forumId=` + result.forumId + `&major=` + result.major + `&year=` + result.grad_year
+                } className="link">
+                <li key={index}>
+                  <div className='result entry'>
+                    <div className="first entry">
+                      {result.forum_name}
+                    </div>
+                    <div className="description entry">
+                      {result.forum_description}
+                    </div>
+                  </div>
+                </li>
+              </Link>
+            )}
+          </div>
+        </ul>
         </div>
         </div>
     );
