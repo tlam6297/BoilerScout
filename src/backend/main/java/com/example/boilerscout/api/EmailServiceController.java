03af3772454f6ee6cd7b7c0e70a35c078a5a96e9
hmontoyag <35666487+hmontoyag@users.noreply.github.com>
2018-04-01 14:04:12 -0500

    Updated to match all the fixes in email&getprofile

--- src/backend/main/java/com/example/boilerscout/api/EmailServiceController.java
+++ src/backend/main/java/com/example/boilerscout/api/EmailServiceController.java
@@ -45,19 +45,20 @@ public class EmailServiceController extends ValidationUtility {
         }
         return sb.toString();
     }
-    public Map<String, Object> sendVerification(@RequestBody Map<String, String> body) {
+       public Map<String, Object> sendVerification(@RequestBody Map<String, String> body) {
 
         Map<String, Object> response = new HashMap<String, Object>();
-        String userId = body.get("id");
-        Integer existingID = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users WHERE user_id='" + userId + "'", Integer.class);
-        if (existingID <= 0) {
-            throw new RuntimeException("[BadRequest] - No user associated with this ID!");
+        String email = body.get("email");
+        Integer existingEmail = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users WHERE email='" + email + "'", Integer.class);
+        //CHECK user exists
+        if (existingEmail <= 0) {
+            throw new RuntimeException("[BadRequest] - No user associated with this email!");
         }
         try {
-            List<Map<String, Object>> email;
+           // List<Map<String, Object>> email;
             //got email
-            email = jdbcTemplate.queryForList("SELECT email from users where user_id =  '" + userId +"'");
-            String to = email.get(0).get("email").toString();
+            String userId = jdbcTemplate.queryForObject("SELECT user_id FROM users WHERE email='" + email + "'", String.class);
+            String to = email;
             //generate new verification code
             //  List<Map<String, Object>> prevVerificationCode = jdbcTemplate.queryForList("SELECT email from users where user_id =  '" + userId +"'");
             // int prev = (Integer) prevVerificationCode.get(0).get("email_verified"); //always is an int so this should be okay
@@ -70,12 +71,11 @@ public class EmailServiceController extends ValidationUtility {
 
 
             String subject ="BoilerScout. Account verification.";
-            String text = "Hi, you are now one step closer to your BoilerScout account.\n\n";
-            text = text + "Please verifiy your account with the following link:\n\n\tlocalhost:8080/verify/get?id=";
-            String id = body.get("id");
+            String text = "Hi,\nYou are now one step closer to your BoilerScout account.\n\n";
+            text = text + "Please verifiy your account with the following link:\n\n\tlocalhost:8080/verify?id=";
             sendSimpleMessage(to,subject,text + userId + "&query=" + verificationCode);
             response.put("ok","ok");
-            response.put("userId",id);
+            response.put("userId",userId);
             return response;
 
 
@@ -89,25 +89,26 @@ public class EmailServiceController extends ValidationUtility {
 
     public Map<String, Object> sendNewPassword(@RequestBody Map<String, String> body){
         Map<String, Object> response = new HashMap<String, Object>();
-        String userId = body.get("id");
-        String token = body.get("token").toString();
-        Integer existingID = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users WHERE user_id='" + userId + "'", Integer.class);
+        String email = body.get("email");
+       // String token = body.get("token").toString();
+        Integer existingEmail = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users WHERE email='" + email + "'", Integer.class);
         //CHECK user exists
-        if (existingID <= 0) {
-            throw new RuntimeException("[BadRequest] - No user associated with this ID!");
+        if (existingEmail <= 0) {
+            throw new RuntimeException("[BadRequest] - No user associated with this email!");
         }
         //CHECK token is valid
-        if (!isValidToken(token, userId) || isExpiredToken(token)) {
-            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR + " - This token is not valid!");
-            return response;
-        }
+      //  if (!isValidToken(token, userId) || isExpiredToken(token)) {
+      //      response.put("status", HttpStatus.INTERNAL_SERVER_ERROR + " - This token is not valid!");
+      //      return response;
+      //  }
         try {
 
             List<Map<String, Object>> userInfo;
             //get email, and current hashed pass
-            userInfo = jdbcTemplate.queryForList("SELECT email, password from users where user_id =  '" + userId +"'");
-            String oldPassword = userInfo.get(0).get("password").toString();
-            String to = userInfo.get(0).get("email").toString();
+            String oldPassword = jdbcTemplate.queryForObject("SELECT password FROM users WHERE email='" + email + "'", String.class);
+            String userId = jdbcTemplate.queryForObject("SELECT user_id FROM users WHERE email='" + email + "'", String.class);
+
+            String to = email;
               BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
               String newPassword;
               String hashedNewPass;
@@ -117,10 +118,11 @@ public class EmailServiceController extends ValidationUtility {
                   hashedNewPass = passwordEncoder.encode(newPassword);
             } while (hashedNewPass.equals(oldPassword));
              //update password in db, hashed already
+            hashedNewPass = '"'+hashedNewPass+'"';
             jdbcTemplate.update("UPDATE users SET password=" + hashedNewPass + " WHERE user_id='" + userId + "'");
 
             String subject ="BoilerScout. New password requested.";
-            String text = "Hi, you have requested a new password for your account.\n";
+            String text = "Hi,\n\n You have requested a new password for your account.\n";
             text = text + "Note that if you have recently requested more than one new password, ";
             text = text + "the one contained in this email might be outdated.  ";
             text = text + "Please ensure this is the most recent email of this kind you have recieved.\n\n";
@@ -130,6 +132,8 @@ public class EmailServiceController extends ValidationUtility {
             //send email with unhashed new pass
             response.put("ok","ok");
             response.put("userId",userId);
+           // response.put("new",hashedNewPass);
+            //response.put("userId",userId);
             return response;
 
         } catch (DataAccessException ex) {
