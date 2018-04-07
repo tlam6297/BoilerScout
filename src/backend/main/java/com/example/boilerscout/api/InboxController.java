74d1ea58ab863b7e3fb77c09891b3b89a067e99d
barisdingil <barisdingil@gmail.com>
2018-04-07 17:17:59 -0400

    Inbox with sorting
    Search function not done yet.

--- src/backend/main/java/com/example/boilerscout/api/InboxController.java
+++ src/backend/main/java/com/example/boilerscout/api/InboxController.java
@@ -26,43 +26,37 @@ public class InboxController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public Map<String, Object> getInbox (@RequestParam String user_Id, @RequestParam int sort){//, @RequestParam String token) {
-
+    public Map<String, Object> getInbox (@RequestParam String user_Id, @RequestParam int sort, @RequestParam String search){
 
         Map<String, Object> response = new HashMap<String, Object>();
+
         response.put("user_Id", user_Id);
         response.put("sort", sort);
-      //  response.put("token", token);
+        response.put("search",search);
 
         try {
+
             List<Map<String, Object>> listOfinbox = jdbcTemplate.queryForList("SELECT * FROM Mes WHERE User_Receiver='" + user_Id + "'");
 
-            if(sort==1) response.put("listofInbox", listOfinbox);
+            if(search.equals("")) {
+                if (sort == 0) viseVersa(listOfinbox);
+                response.put("listofInbox", listOfinbox);
 
-            else if(sort ==0) {
+            } else {
 
-//                    List<Map<String, Object>> listOfinboxR = listOfinbox;
-//                    System.out.print(listOfinbox);
-                    int j = 0;
-                    int i = listOfinbox.size()-1;
-                    while( i >= (listOfinbox.size())/2 ) {
-                        Map<String, Object> temp = listOfinbox.get(j);
-                        listOfinbox.set(j, listOfinbox.get(i));
-                        listOfinbox.set(i--, temp);
-                        j++;
-
-
-//                        listOfinbox.set(j++, listOfinboxR.get(i--));
-//
-//                        response.put("sizeR", listOfinboxR.size());
-//                        response.put("size", listOfinbox.size());
-
+                List<Map<String, Object>> listSearch = null;
+                listOfinbox.toArray();
+                for(int i=0;i < listOfinbox.size();i++){
+                    String[] sentence = (listOfinbox.get(i).toString()).split(" ");
+                    for(int j=0;j<sentence.length;j++){
+                        if(sentence[j].compareTo(search)==1){
+                            listSearch.set(1, listOfinbox.get(i));
+                        }
                     }
-                    response.put("listofInbox", listOfinbox);
-                response.put("iteration",j);
-                response.put("iterationi",i);
-
                 }
+                response.put("listSearch", listOfinbox);
+            }
+
             response.put("status", HttpStatus.OK);
 
         } catch(DataAccessException e) {
@@ -72,7 +66,16 @@ public class InboxController {
             throw new RuntimeException("[InternalServerError] - Error accessing data.");
 
         }
-
         return response;
+    }
+
+    public List<Map<String, Object>> viseVersa(List<Map<String, Object>> a){
+        for (int i = a.size() - 1, j = 0; i >= (a.size()) / 2;) {
+            Map<String, Object> temp = a.get(j);
+            a.set(j++, a.get(i));
+            a.set(i--, temp);
+        }
+
+        return a;
     }
 }
