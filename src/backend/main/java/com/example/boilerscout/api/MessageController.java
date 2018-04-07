2fdcddb754fcca97eb5a5110759d279c40508f3f
barisdingil <barisdingil@gmail.com>
2018-04-07 17:15:42 -0400

    MessageController updated with Token Validation
    Checking e-mail of the message receiver and saving its user_id to the database.

--- src/backend/main/java/com/example/boilerscout/api/MessageController.java
+++ src/backend/main/java/com/example/boilerscout/api/MessageController.java
@@ -1,7 +1,5 @@
 package com.example.boilerscout.api;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,48 +9,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
-import javax.xml.bind.DatatypeConverter;
 import java.util.*;
 
 /*
 Created  by Baris Dingil
- */
+*/
 
 @Service
-public class MessageController {
+public class MessageController extends ValidationUtility {
+
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public Map<String, Object> getMessage(@RequestBody Map<String, String> body){
+    public Map<String, Object> getMessage(@RequestBody Map<String, String> body) {
 
         Map<String, Object> response = new HashMap<String, Object>();
+        String dest = body.get("User_Receiver");
+        String message = body.get("message");
+        String sender = body.get("sender");
+        String token = body.get("token");
 
-        try {
-            String dest = body.get("User_Receiver");
-            String message = body.get("Message");
-            String sender = body.get("Sender");
+        if (!isValidToken(token, sender) || isExpiredToken(token)) {
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR + " - This token is not valid!");
+            return response;
+        } else {
+            try {
 
-            List<Map<String, Object>> existingUser = jdbcTemplate.queryForList("SELECT * FROM users WHERE User_Receiver='" + dest + "'");
 
-            if (existingUser.size() == 0) {
-                throw new RuntimeException("User don't exist, check the destination again!");
+                List<Map<String, Object>> existingUser = jdbcTemplate.queryForList("SELECT user_id FROM users WHERE email ='" + dest + "'");
+
+                if (existingUser.size() == 0) throw new RuntimeException("User don't exist!");
+
+                String user_ = jdbcTemplate.queryForObject("SELECT user_id FROM users WHERE email ='" + dest + "'", String.class);
+                jdbcTemplate.update("INSERT INTO Mes (User_Receiver, message, sender) VALUES (?, ?, ?)", user_, message, sender);
+
+                response.put("status", HttpStatus.OK);
+
+
+            } catch (DataAccessException e) {
+
+                log.info("Exception Message" + e.getMessage());
+                response.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+                throw new RuntimeException("[InternalServerError] - Error accessing data.");
+
             }
 
-            jdbcTemplate.update("INSERT INTO Messages (User_Receiver, message, sender) VALUES (?, ?, ?)", dest, message, sender);
-
-
-            response.put("status", HttpStatus.OK);
             return response;
-        }catch(DataAccessException e){
-
         }
-        return response;
     }
-
-
 }
