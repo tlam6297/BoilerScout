2701daf1966b0a21a246dba766262b6f7d8b77ab
Jacob Miecznikowski <jmieczni@users.noreply.github.com>
2018-04-06 21:07:18 -0400

    View/forum (#30)
    * Add Advanced Filters Box
    
    * Create ForumSearch View
    
    * Create Forum View
    
    * Finish Creating Forum without Backend functionality
    
    * GET to display threads and small formatting
    
    * Add Inform the user when not found
    
    * Complete Search Filter for Forum

--- src/frontend/boilerscout/src/Scout.js
+++ src/frontend/boilerscout/src/Scout.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
-import { Button, FormGroup, FormControl, ControlLabel, Radio } from "react-bootstrap";
+import { Button, FormGroup, FormControl, ControlLabel, Radio, DropdownButton, MenuItem } from "react-bootstrap";
 import './Scout.css'
 import TopNavBar from './TopNavBar'
 import GETRequest from './GETRequest'
-// import Popup from 'react-popup'
 import axios from 'axios';
 
 class Scout extends Component {
@@ -21,6 +20,11 @@ class Scout extends Component {
       showResult: false,
       submitClicked: false,
       redirect: false,
+      advanced: false,
+      advText: "ADVANCED FILTERS",
+      selectedMajor: "Computer Science",
+      selectedYear: 2018,
+      noResults: true,
       results: [],
       posts: [],
     };
@@ -29,6 +33,18 @@ class Scout extends Component {
   handleChange = (event) => {
     this.setState({
       [event.target.id]: event.target.value
+    });
+  }
+
+  handleChangeYear = (e) => {
+    this.setState({
+      selectedYear: e.target.value,
+    });
+  }
+
+  handleChangeMajor = (e) => {
+    this.setState({
+      selectedMajor: e.target.value,
     });
   }
 
@@ -41,7 +57,7 @@ class Scout extends Component {
       })
       return;
     }
-
+    
     const id = this.getLocalStorage("id");
     let token = this.getLocalStorage("token");
 
@@ -57,7 +73,14 @@ class Scout extends Component {
       if (res.data.status == "OK") {
         this.setState({
           results: res.data.query,
-        });        
+        });
+
+        // if results is longer than 0, means we do have results
+        if (res.data.query.length > 0) {
+          this.setState({
+            noResults: false,
+          });      
+        }  
       } else {
         alert("Invalid Token- Please login again");
         this.setState({
@@ -80,7 +103,7 @@ class Scout extends Component {
               <Link to={
                   `/profile?user_id=` + result.user_id + `&name=` + result.full_name + `&bio=` + result.bio + `&major=` + result.major + `&year=` + result.grad_year
                 } className="link">
-                <li key={index}>
+                <li key={index} className="listelem">
                   <div className='result entry'>
                     <div className="first entry">
                       {result.full_name}
@@ -99,6 +122,58 @@ class Scout extends Component {
         </ul>
       </div>
     )    
+  }
+
+  renderAdvanced = () => {
+    const majors = [
+      "Computer Science",
+      "Anthropology",
+      "Mechanical Engineering",
+      "Chemical Engineering",
+      "Biology",
+      "Management",
+      "Computer Engineering",
+      "Materials Engineering",
+    ];
+
+    const grad_years = [
+      2018,
+      2019,
+      2020,
+      2021,
+      2022,
+      2023,
+      2024,
+      2025,
+      2026,
+      2027,
+      2028,
+      2029,
+      2030,
+    ];
+
+    if (this.state.advanced) {
+      return (
+        <div className="advanced">
+          <div className="grad_year">
+            <h3>Graduation Year:</h3>
+            <select value={this.state.selectedYear} onChange={this.handleChangeYear} name="dropdownYear" id="0" className="select">
+              {grad_years.map((year, i) =>                
+                <option className="select" value={year}>{year}</option>
+              )}
+            </select>
+          </div>
+          <div className="major">
+            <h2>Major:</h2>
+            <select value={this.state.selectedMajor} onChange={this.handleChangeMajor} name="dropdownMajor" id="1" className="select">
+              {majors.map((major, i) =>
+                <option value={major}>{major}</option>
+              )}
+            </select>
+          </div>
+        </div>
+      )
+    }
   }
 
   handleRadioName = (event) => {
@@ -129,7 +204,7 @@ class Scout extends Component {
         <div className="navs">
           <TopNavBar/>
         </div>        
-        <form onSubmit={this.handleSubmit} className="form">
+        <form onSubmit={this.handleSubmit} className="form">  
         <FormGroup controlId="searchinput" bsSize="large">
           <FormControl
             className="FormInput ScoutForm"
@@ -155,6 +230,31 @@ class Scout extends Component {
               </Radio>
             </FormGroup>
           </div>
+
+          <div className="advancedButton">
+            <Route render={({ history}) => (
+              <button
+                type='button'
+                onClick={() => {
+
+                  // Change button text
+                  if (this.state.advText == "ADVANCED FILTERS") {
+                    this.setState({
+                      advText: "SIMPLE SEARCH",
+                    })
+                  } else {
+                    this.setState({
+                      advText: "ADVANCED FILTERS",
+                    })
+                  }
+
+                  this.setState({
+                    advanced: !this.state.advanced,
+                  });
+                }}
+              >{this.state.advText}</button>
+            )} />
+          </div>
           <Button
               block
               bsSize="small"
@@ -164,19 +264,12 @@ class Scout extends Component {
           <p></p>
         </form>
 
+        {this.renderAdvanced()}
+
         { this.renderResults() }
         {this.state.redirect && (
           <Redirect to={'/login'}/>   
         )}
-
-        <div className="advanced">
-          <Route render={({ history}) => (
-            <button
-              type='button'
-              onClick={() => { history.push('/advanced-filters') }}
-            >ADVANCED FILTERS</button>
-          )} />
-        </div>
       </div>
     );
   }
