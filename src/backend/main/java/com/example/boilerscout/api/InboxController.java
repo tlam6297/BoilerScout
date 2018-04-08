a535f90164ef0109224aa83bacdc81257725cc2f
barisdingil <barisdingil@gmail.com>
2018-04-07 20:27:33 -0400

    Update InboxController
    User can reach Inbox/Outbox, search in Inbox/Outbox, sort messages by date.

--- src/backend/main/java/com/example/boilerscout/api/InboxController.java
+++ src/backend/main/java/com/example/boilerscout/api/InboxController.java
@@ -3,7 +3,81 @@ package com.example.boilerscout.api;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import javafx.application.Application;
+import org.slf4j.Logger;package com.example.boilerscout.api;
+
+import javafx.application.Application;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.*;
+
+@Service
+public class InboxController {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Map<String, Object> getInbox (@RequestParam String user_Id, @RequestParam int sort, @RequestParam String search,@RequestParam int inorout){
+
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        response.put("user_Id", user_Id);
+        response.put("sort", sort);
+        response.put("search",search);
+        //if inorout is 0 return Outbox, if inorout is 1 return Inbox
+        response.put("inorout",inorout);
+
+        try {
+
+              List<Map<String, Object>> listOfinbox = jdbcTemplate.queryForList("SELECT * FROM Mes WHERE User_Receiver='" + user_Id + "'");
+              if(inorout==0) listOfinbox = jdbcTemplate.queryForList("SELECT * FROM Mes WHERE sender='" + user_Id + "'");
+
+            // if nothing is searched return every message
+            if(search.equals("")) {
+                if (sort == 0) viseVersa(listOfinbox);
+                response.put("listofInbox", listOfinbox);
+
+            } else {
+            //search in messages if "search" exists
+                List<Map<String, Object>> listSearch = jdbcTemplate.queryForList("SELECT * FROM Mes WHERE message LIKE '%"+search+"%' AND User_Receiver='"+user_Id+"'" );
+                if(inorout==0) listSearch = jdbcTemplate.queryForList("SELECT * FROM Mes WHERE message LIKE '%"+search+"%' AND sender='"+user_Id+"'" );
+
+                if(sort==0) viseVersa(listSearch);
+                response.put("listSearch", listSearch);
+            }
+
+            response.put("status", HttpStatus.OK);
+
+        } catch(DataAccessException e) {
+
+            log.info("Exception Message" + e.getMessage());
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new RuntimeException("[InternalServerError] - Error accessing data.");
+
+        }
+        return response;
+    }
+
+    public List<Map<String, Object>> viseVersa(List<Map<String, Object>> a){
+        for (int i = a.size() - 1, j = 0; i >= (a.size()) / 2;) {
+            Map<String, Object> temp = a.get(j);
+            a.set(j++, a.get(i));
+            a.set(i--, temp);
+        }
+
+        return a;
+    }
+}
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
