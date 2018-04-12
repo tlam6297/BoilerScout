3897fab6dc5f40c65e2a4b92a7b713458194d41a
Terry Lam <tlam2297@gmail.com>
2018-04-12 16:51:19 -0400

    FIX: Add missing RequestParam import

--- src/backend/main/java/com/example/boilerscout/api/ProfileController.java
+++ src/backend/main/java/com/example/boilerscout/api/ProfileController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
 
