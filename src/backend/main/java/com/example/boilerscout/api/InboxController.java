36b151f4c4eb403e05f07b736ff7be07c72d06af
barisdingil <barisdingil@gmail.com>
2018-04-14 17:39:06 -0400

    Add files via upload

---
+++ src/backend/main/java/com/example/boilerscout/api/InboxController.java
@@ -0,0 +1,62 @@
+package com.example.boilerscout.api;
+
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.*;
+
+@Service
+public class InboxController2 extends ValidationUtility {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Map<String, Object> getInbox (@RequestParam String user_Id, @RequestParam String sort,@RequestParam String token) {
+
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        response.put("user_Id", user_Id);
+        response.put("token", token);
+        response.put("sort", sort);
+
+        if (!isValidToken(token, user_Id) || isExpiredToken(token)) {
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR + " - This token is not valid!");
+            return response;
+
+        } else {
+            try {
+                List<Map<String, Object>> listOfinbox = jdbcTemplate.queryForList("SELECT Mes.message,profiles.full_name,users.email,Mes.datesent FROM Mes INNER JOIN profiles ON Mes.sender=profiles.user_id INNER JOIN users ON users.user_id=Mes.sender WHERE User_Receiver='"+user_Id+"'ORDER BY datesent ASC");
+                //List<Map<String, Object>>   listOfinbox = jdbcTemplate.queryForList("SELECT Mes.message,profiles.full_name,Mes.emailsender,Mes.dateString FROM Mes INNER JOIN profiles ON Mes.sender=profiles.user_id WHERE User_Receiver='"+user_Id+"'ORDER BY datesent ASC");
+              //  List<Map<String, Object>> listOfinbox = jdbcTemplate.queryForList("SELECT message, emailsender, dateString  FROM Mes  WHERE User_Receiver='" + user_Id + "'ORDER BY datesent ASC");
+
+                if (sort.equals("DESC")) {
+                    listOfinbox = jdbcTemplate.queryForList("SELECT Mes.message,profiles.full_name,users.email,Mes.datesent FROM Mes INNER JOIN profiles ON Mes.sender=profiles.user_id INNER JOIN users ON users.user_id=Mes.sender WHERE User_Receiver='"+user_Id+"'ORDER BY datesent DESC ");
+                   // listOfinbox = jdbcTemplate.queryForList("SELECT Mes.message,profiles.full_name,Mes.emailsender,Mes.dateString FROM Mes INNER JOIN profiles ON Mes.User_Receiver=profiles.user_id WHERE User_Receiver='"+user_Id+"'ORDER BY datesent DESC");
+                    //listOfinbox = jdbcTemplate.queryForList("SELECT message, emailsender, dateString  FROM Mes WHERE User_Receiver='" + user_Id + "'ORDER BY datesent DESC");
+                }
+
+                response.put("listOfinbox", listOfinbox);
+                response.put("status", HttpStatus.OK);
+
+            } catch (DataAccessException e) {
+
+                log.info("Exception Message" + e.getMessage());
+                response.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+                throw new RuntimeException("[InternalServerError] - Error accessing data.");
+
+            }
+
+            return response;
+        }
+    }
+}
