0b54514d5931cece6ec723d423f21776cfcdbe8b
Selin Ovali <selinovali@Selins-MacBook-Pro.local>
2018-04-16 17:13:34 -0400

    Added Graduation & Major dropdowns to Sign Up

--- src/frontend/boilerscout/src/SignUp.js
+++ src/frontend/boilerscout/src/SignUp.js
@@ -7,16 +7,18 @@ import POSTRequest from './POSTRequest'
 
 class SignUp extends Component {
   constructor (props) {
-    super(props);
+      super(props);
+      this.majorDropdown = this.majorDropdown.bind(this);
 
     this.state = {
       email: "",
       password: "",
       repeatpassword: "",
       fullname: "",
-      major: "",
-      grad: "",
+      Major: "",
+      Graduation: "",
       redirect: false,
+      allmajors: ["Accounting","Acting","Actuarial Science","Aeronautical And Astronautical Engineering","Aeronautical Engineering Technology","Aerospace Financial Analysis","African American Studies","Agribusiness (Multiple Concentrations)","Agricultural Communication","Agricultural Economics (Multiple Concentrations)","Agricultural Education","Agricultural Engineering","Agricultural Engineering","Agricultural Systems Management","Agronomy (Multiple Concentrations)","Airline Management And Operations","Airport Management And Operations","American Studies","Animal Sciences (Multiple Concentrations)","Animation","Anthropology","Applied Exercise And Health (Pre)","Applied Meteorology And Climatology","Art History","Asian Studies","Athletic Training (Pre)","Atmospheric Science/Meteorology","Audio Engineering Technology","Automation And Systems Integration Engineering Technology","Aviation Management","Biochemistry","Biochemistry (Biology)","Biochemistry (Chemistry)","Biological Engineering - Multiple Concentrations","Biological Engineering - Multiple Concentrations","Biology","Biomedical Engineering","Brain And Behavioral Sciences","Building Information Modeling","Cell, Molecular, And Developmental Biology","Chemical Engineering","Chemistry","Chemistry - American Chemical Society","Chinese Studies","Civil Engineering","Classical Studies","Communication, General (Pre)","Comparative Literature","Computer And Information Technology","Computer Engineering","Computer Science","Construction Engineering","Construction Management Technology","Creative Writing","Crop Science","Cybersecurity","Data Science","Data Visualization","Design And Construction Integration","Developmental And Family Science","Early Childhood Education And Exceptional Needs","Ecology, Evolution, And Environmental Sciences","Economics (School Of Management)","Electrical Engineering","Electrical Engineering Technology","Elementary Education","Engineering / Technology Teacher Education","English","Environmental And Ecological Engineering","Environmental And Natural Resources Engineering","Environmental And Natural Resources Engineering","Environmental Geosciences","Environmental Health Sciences","Environmental Studies (Pre)","Exploratory Studies","Family And Consumer Sciences Education","Farm Management","Film And Theatre Production","Film And Video Studies","Finance","Financial Counseling And Planning","Fisheries And Aquatic Sciences","Flight (Professional Flight Technology)","Foods And Nutrition In Business","Food Science","Forestry","French","Game Development And Design","General Education: Curriculum And Instruction (Non-Licensure)","General Education: Educational Studies (Non-Licensure)","Genetic Biology","Geology And Geophysics","German","Global Studies","Health And Disease","Health Sciences - Preprofessional","History","Horticulture (Multiple Concentrations)","Hospitality And Tourism Management","Human Resource Development","Human Services","Industrial (Consumer Product) Design","Industrial Engineering","Industrial Engineering Technology","Industrial Management","Insect Biology","Integrated Studio Arts","Interdisciplinary Engineering Studies","Interior (Space Planning) Design","Italian Studies","Japanese","Jewish Studies","Kinesiology","Landscape Architecture (Pre)","Law And Society","Learning Sciences In Educational Studies (Non Licensure)","Linguistics","Management (General)","Marketing","Materials Engineering","Mathematics","Mathematics Education","Mechanical Engineering","Mechanical Engineering Technology","Mechatronics Engineering Technology","Medical Laboratory Sciences","Microbiology","Multidisciplinary Engineering","Natural Resources And Environmental Science (Multiple Concentrations)","Network Engineering Technology","Neurobiology And Physiology","Nuclear Engineering","Nursing","Nutrition, Fitness, And Health","Nutrition And Dietetics","Nutrition And Dietetics/Nutrition, Fitness And Health (Dual Major)","Nutrition Science","Occupational Health Science","Organizational Leadership","Pharmacy","Philosophy","Physics","Planetary Sciences","Plant Genetics, Breeding, And Biotechnology","Plant Science","Political Science","Predentistry","Prelaw","Premedicine","Preoccupational Therapy","Prephysical Therapy","Prephysician's Assistant","Pre-Veterinary Medicine","Professional Writing","Psychological Sciences","Public Health","Purdue Polytechnic Institute Statewide Programs","Radiological Health Sciences - Health Physics","Radiological Health Sciences - Pre-Medical Physics","Religious Studies","Retail Management","Robotics Engineering Technology","Russian","Sales And Marketing","Science Education (Biology, Chemistry, Earth/Space, Physics)","Selling And Sales Management","Social Studies Education","Sociology","Soil And Water Sciences","Sound For The Performing Arts","Spanish","Special Education: Elementary Education And Special Education - Mild Intervention","Special Education: Mild And Intense Intervention P-12","Special Education: Mild Intervention P-12","Speech, Language, And Hearing Sciences","Statistics - Applied Statistics","Statistics With Mathematics Option","Studio Arts And Technology","Supply Chain Information And Analytics","Supply Chain Management Technology","Sustainable Biomaterials – Process And Product Design","Sustainable Food And Farming Systems","Systems Analysis And Design","Theatre","Theatre Design And Production","Transdisciplinary Studies In Engineering Technology","Transdisciplinary Studies In Technology","Turf Management And Science","Undecided Liberal Arts","Undecided Within Engineering","Unmanned Aerial Systems","Ux Design","Veterinary Technician Or Technologist","Virtual Product Integration","Visual Arts Design Education","Visual Arts Education","Visual Communications Design","Visual Effects Compositing","Web Programming And Design","Wildlife","Women’s, Gender And Sexuality Studies"],  
     };
   }
 
@@ -35,10 +37,31 @@ class SignUp extends Component {
     return (validEmail && validPassword && repeatPassword);
   }
 
+  majorDropdown = () => {
+    var majors = [
+      "Accounting","Acting","Actuarial Science","Aeronautical And Astronautical Engineering","Aeronautical Engineering Technology","Aerospace Financial Analysis","African American Studies","Agribusiness (Multiple Concentrations)","Agricultural Communication","Agricultural Economics (Multiple Concentrations)","Agricultural Education","Agricultural Engineering","Agricultural Engineering","Agricultural Systems Management","Agronomy (Multiple Concentrations)","Airline Management And Operations","Airport Management And Operations","American Studies","Animal Sciences (Multiple Concentrations)","Animation","Anthropology","Applied Exercise And Health (Pre)","Applied Meteorology And Climatology","Art History","Asian Studies","Athletic Training (Pre)","Atmospheric Science/Meteorology","Audio Engineering Technology","Automation And Systems Integration Engineering Technology","Aviation Management","Biochemistry","Biochemistry (Biology)","Biochemistry (Chemistry)","Biological Engineering - Multiple Concentrations","Biological Engineering - Multiple Concentrations","Biology","Biomedical Engineering","Brain And Behavioral Sciences","Building Information Modeling","Cell, Molecular, And Developmental Biology","Chemical Engineering","Chemistry","Chemistry - American Chemical Society","Chinese Studies","Civil Engineering","Classical Studies","Communication, General (Pre)","Comparative Literature","Computer And Information Technology","Computer Engineering","Computer Science","Construction Engineering","Construction Management Technology","Creative Writing","Crop Science","Cybersecurity","Data Science","Data Visualization","Design And Construction Integration","Developmental And Family Science","Early Childhood Education And Exceptional Needs","Ecology, Evolution, And Environmental Sciences","Economics (School Of Management)","Electrical Engineering","Electrical Engineering Technology","Elementary Education","Engineering / Technology Teacher Education","English","Environmental And Ecological Engineering","Environmental And Natural Resources Engineering","Environmental And Natural Resources Engineering","Environmental Geosciences","Environmental Health Sciences","Environmental Studies (Pre)","Exploratory Studies","Family And Consumer Sciences Education","Farm Management","Film And Theatre Production","Film And Video Studies","Finance","Financial Counseling And Planning","Fisheries And Aquatic Sciences","Flight (Professional Flight Technology)","Foods And Nutrition In Business","Food Science","Forestry","French","Game Development And Design","General Education: Curriculum And Instruction (Non-Licensure)","General Education: Educational Studies (Non-Licensure)","Genetic Biology","Geology And Geophysics","German","Global Studies","Health And Disease","Health Sciences - Preprofessional","History","Horticulture (Multiple Concentrations)","Hospitality And Tourism Management","Human Resource Development","Human Services","Industrial (Consumer Product) Design","Industrial Engineering","Industrial Engineering Technology","Industrial Management","Insect Biology","Integrated Studio Arts","Interdisciplinary Engineering Studies","Interior (Space Planning) Design","Italian Studies","Japanese","Jewish Studies","Kinesiology","Landscape Architecture (Pre)","Law And Society","Learning Sciences In Educational Studies (Non Licensure)","Linguistics","Management (General)","Marketing","Materials Engineering","Mathematics","Mathematics Education","Mechanical Engineering","Mechanical Engineering Technology","Mechatronics Engineering Technology","Medical Laboratory Sciences","Microbiology","Multidisciplinary Engineering","Natural Resources And Environmental Science (Multiple Concentrations)","Network Engineering Technology","Neurobiology And Physiology","Nuclear Engineering","Nursing","Nutrition, Fitness, And Health","Nutrition And Dietetics","Nutrition And Dietetics/Nutrition, Fitness And Health (Dual Major)","Nutrition Science","Occupational Health Science","Organizational Leadership","Pharmacy","Philosophy","Physics","Planetary Sciences","Plant Genetics, Breeding, And Biotechnology","Plant Science","Political Science","Predentistry","Prelaw","Premedicine","Preoccupational Therapy","Prephysical Therapy","Prephysician's Assistant","Pre-Veterinary Medicine","Professional Writing","Psychological Sciences","Public Health","Purdue Polytechnic Institute Statewide Programs","Radiological Health Sciences - Health Physics","Radiological Health Sciences - Pre-Medical Physics","Religious Studies","Retail Management","Robotics Engineering Technology","Russian","Sales And Marketing","Science Education (Biology, Chemistry, Earth/Space, Physics)","Selling And Sales Management","Social Studies Education","Sociology","Soil And Water Sciences","Sound For The Performing Arts","Spanish","Special Education: Elementary Education And Special Education - Mild Intervention","Special Education: Mild And Intense Intervention P-12","Special Education: Mild Intervention P-12","Speech, Language, And Hearing Sciences","Statistics - Applied Statistics","Statistics With Mathematics Option","Studio Arts And Technology","Supply Chain Information And Analytics","Supply Chain Management Technology","Sustainable Biomaterials – Process And Product Design","Sustainable Food And Farming Systems","Systems Analysis And Design","Theatre","Theatre Design And Production","Transdisciplinary Studies In Engineering Technology","Transdisciplinary Studies In Technology","Turf Management And Science","Undecided Liberal Arts","Undecided Within Engineering","Unmanned Aerial Systems","Ux Design","Veterinary Technician Or Technologist","Virtual Product Integration","Visual Arts Design Education","Visual Arts Education","Visual Communications Design","Visual Effects Compositing","Web Programming And Design","Wildlife","Women’s, Gender And Sexuality Studies"
+    ]
+   
+    var dropdown = document.getElementById("Major");
+    for (var i = 0; i < majors.length; i++) {
+      var curr = majors[i];
+      var el = document.createElement("option");
+      el.textContent = curr;
+      el.value = curr;
+      //dropdown.appendChild(el);
+      console.log("uh oh");
+      console.log(dropdown);
+      console.log("uh oh");
+    }
+   
+   }
+
   handleChange = (event) => {
     this.setState({
       [event.target.id]: event.target.value
     });
+
+    console.log(event.target.value);
   }
 
   handleSubmit = (event) => {
@@ -138,15 +161,28 @@ class SignUp extends Component {
               />
             </FormGroup>
             <FormGroup controlId="grad" bsSize="large">
-              <ControlLabel>Grad Year:</ControlLabel>
-              <FormControl
-                className="FormInput"
-                autoFocus
-                type="text"
-                value={this.state.grad}
-                onChange={this.handleChange}
-              />
+            <ControlLabel>Graduation Year:</ControlLabel>
+              <select
+                className="FormInput graduation"
+                id="Graduation"
+                value={this.state.Graduation}
+                onChange={this.handleChange}>
+                <option value="2019">2019</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+              </select>
             </FormGroup>
+            <ControlLabel>Major:</ControlLabel>
+                <select
+                  className="FormInput major"
+                  id="Major"
+                  value={this.state.Major}
+                  onChange={this.handleChange}>
+                  {this.state.allmajors.map((result, index) =>
+                  <option value={result}>{result}</option>
+                  )}
+                </select>
             <FormGroup controlId="email" bsSize="large">
               <ControlLabel>Email:</ControlLabel>
               <FormControl
