ac76bdcc1d9cdee077372f7569edf87bc2cf196f
Terry Lam <tlam2297@gmail.com>
2018-04-19 00:28:42 -0400

    CHORE: More tests for forum spec

--- src/test/java/com/example/boilerscout/api/ForumControllerSpec.java
+++ src/test/java/com/example/boilerscout/api/ForumControllerSpec.java
@@ -7,13 +7,21 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
@@ -29,6 +37,21 @@ public class ForumControllerSpec {
 
     private MockMvc mvc;
 
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
@@ -36,11 +59,18 @@ public class ForumControllerSpec {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    String userId = "bee4f10f-8f1f-43ee-b54f-a810f853cae9";
 
-    String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsYW00NUBwdXJkdWUuZWR1IiwidXNlcklkIjoiYmVlNGYxMGYtOGYxZi00M2VlLWI1N" +
-            "GYtYTgxMGY4NTNjYWU5IiwiZXhwIjoxNTI0NjgxNzg3fQ.PWdkHvZZ12l3wymI0pasVa3TQ0IGGhptvZ1-PQkxlNLXq8q36tFIXkB2Jvtk" +
-            "P5GvfeW1xRhYHS-mtQoILVV5eg";
+    //Static values, may need to be updated?
+    //TODO create a token that never expires
+    String userId = "2c399169-744e-4eda-9f1c-c0723dcc761d";
+
+    String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsYW00NUBwdXJkdWUuZWR1IiwidXNlcklkIjoiMmMzOTkxNjktNzQ0ZS00ZWRhLTlmMW" +
+            "MtYzA3MjNkY2M3NjFkIiwiZXhwIjoxNTI0OTQyMDQyfQ.Q_oNfVaNaI3HJpe0VFD1YUY-SJG8UphzvYhIIx_YM3CBliIBY12UVnoPjb6v-" +
+            "3DE7wtFagSSLkbGs3zPWuJjEQ";
+
+    String forumId = "528fc18a-ebbc-4b0a-9ca3-bd00d6db006c";
+
+    String threadId = "00a4aca0-27cd-44d1-8326-f0a852fc43c8";
 
     @Before
     public void setup() throws Exception {
@@ -58,16 +88,75 @@ public class ForumControllerSpec {
     }
 
     @Test
-    public void forumAccess() throws Exception {
-        mvc.perform(get("/community")
+    public void validStartThread() throws Exception {
+        Map<String, Object> startThreadMap = new HashMap<String, Object>();
+        startThreadMap.put("userId", userId);
+        startThreadMap.put("token", token);
+        startThreadMap.put("forumId", forumId);
+        startThreadMap.put("threadTitle", "someThreadTitle");
+        startThreadMap.put("threadBody", "someThreadBody");
 
-        .param("userId", "d")
-        .param("token", "d")
-        ).andExpect(mvcResult -> )
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(content()
-                    .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+        String startThreadJson = json(startThreadMap);
+        this.mvc.perform(post("/community/start-thread")
+        .contentType(contentType)
+        .content(startThreadJson)).andExpect(status().isOk());
     }
+
+    @Test
+    public void validRetrieveThreads() throws Exception {
+        mvc.perform(get("/community/get-threads")
+        .param("userId", userId)
+        .param("token", token)
+        .param("forumId", forumId))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void validViewThread() throws Exception {
+        mvc.perform(get("/community/view-thread")
+        .param("userId", userId)
+        .param("token", token)
+        .param("threadId", threadId))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void validPostReply() throws Exception {
+        Map<String, Object> postReplyMap = new HashMap<String, Object>();
+        postReplyMap.put("userId", userId);
+        postReplyMap.put("token", token);
+        postReplyMap.put("threadId", threadId);
+        postReplyMap.put("postBody", "TERRY CARRIED CS307");
+        String postReplyJson = json(postReplyMap);
+        mvc.perform(post("/community/post-reply")
+        .contentType(contentType)
+        .content(postReplyJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void validForumUpdate() throws Exception {
+        Map<String, Object> updateMap = new HashMap<String, Object>();
+        updateMap.put("forumName", "someForum");
+        updateMap.put("description", "someDescription");
+        updateMap.put("action", "insert");
+        String updateJson = json(updateMap);
+        mvc.perform(post("/community/update")
+        .contentType(contentType)
+        .content(updateJson))
+                .andExpect(status().isOk());
+
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+
+
 
 }
